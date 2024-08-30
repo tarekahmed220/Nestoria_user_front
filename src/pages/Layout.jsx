3df63f546c832347419159a902b0e2f2e569eaf8
AhmedTarek22 <ahmedtarekabdo22@gmail.com>
2024-08-30 08:05:32 +0300

    footer

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Navbar2 from "../components/Navbar2";
+import Footer from "../components/Footer";
 
 function Layout() {
   return (
@@ -8,7 +9,7 @@ function Layout() {
       <Navbar />
       <Navbar2></Navbar2>
       <Outlet />
-      <h2>footer</h2>
+      <Footer></Footer>
     </>
   );
 }

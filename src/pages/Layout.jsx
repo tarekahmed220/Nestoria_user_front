247922502db8500c70c25f96a9047ebd43451126
AhmedTarek22 <ahmedtarekabdo22@gmail.com>
2024-08-30 07:34:32 +0300

    update navbar

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Navbar2 from "../components/Navbar2";
 
 function Layout() {
   return (
     <>
-      <h2 className="text-5xl font-bold text-green-600">nav</h2>
+      <Navbar />
+      <Navbar2></Navbar2>
       <Outlet />
       <h2>footer</h2>
     </>

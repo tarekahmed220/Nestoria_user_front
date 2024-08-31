23a7b3ba84b01d1cfaa1f629a721958c1f3581eb
tarekahmed220 <tarek.ahm12@gmail.com>
2024-09-01 00:22:42 +0300

    complete home page

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -8,48 +8,50 @@ import { Link } from "react-router-dom";
 
 function Navbar2() {
   return (
-    <div className="bg-transparent w-full mx-auto flex items-center justify-between mt-10 px-4 lg:w-[1440px]">
-      <div className="text-white text-2xl lg:text-3xl hover:text-[--mainColor] transition-all duration-150 ">
-        <Link to="/">Nestoria</Link>
-      </div>
-      <ul className="hidden lg:flex justify-between gap-4 lg:gap-12">
-        <li className="text-white hover:text-[--mainColor] transition-all duration-150">
-          <Link to="/">HOME</Link>
-        </li>
-        <li className="text-white hover:text-[--mainColor] transition-all duration-150">
-          <Link to="">PAGES</Link>
-        </li>
-        <li className="text-white hover:text-[--mainColor] transition-all duration-150">
-          <Link href="">SHOP</Link>
-        </li>
-        <li className="text-white hover:text-[--mainColor] transition-all duration-150">
-          <Link href="">COLLECTION</Link>
-        </li>
-        <li className="text-white hover:text-[--mainColor] transition-all duration-150">
-          <Link to="/aboutus">ABOUT US</Link>
-        </li>
-        <li className="text-white hover:text-[--mainColor] transition-all duration-150">
-          <Link to="/contactus">CONTACT US</Link>
-        </li>
-        <li className="text-white hover:text-[--mainColor] transition-all duration-150">
-          <Link to="/register">REGISTER</Link>
-        </li>
-      </ul>
-      <div>
-        <ul className="flex gap-4 lg:gap-5">
-          <FontAwesomeIcon
-            className="text-xl lg:text-2xl text-white hover:text-[--mainColor] transition-all duration-150"
-            icon={faMagnifyingGlass}
-          />
-          <FontAwesomeIcon
-            className="text-xl lg:text-2xl text-white hover:text-[--mainColor] transition-all duration-150"
-            icon={faCartPlus}
-          />
-          <FontAwesomeIcon
-            className="text-xl lg:text-2xl text-white hover:text-[--mainColor] transition-all duration-150"
-            icon={faCaretDown}
-          />
+    <div className="bg-transparent absolute top-[52] left-0  w-full mt-3">
+      <div className=" container flex justify-between items-center w-[1440px] mx-auto">
+        <div className="text-white text-2xl lg:text-3xl hover:text-[--mainColor] transition-all duration-150 font-serif">
+          <Link to="/">Nestoria</Link>
+        </div>
+        <ul className="hidden lg:flex justify-between gap-4 lg:gap-12">
+          <li className="text-white hover:text-[--mainColor] transition-all duration-150">
+            <Link to="/">HOME</Link>
+          </li>
+          <li className="text-white hover:text-[--mainColor] transition-all duration-150">
+            <Link to="">PAGES</Link>
+          </li>
+          <li className="text-white hover:text-[--mainColor] transition-all duration-150">
+            <Link href="">SHOP</Link>
+          </li>
+          <li className="text-white hover:text-[--mainColor] transition-all duration-150">
+            <Link href="">COLLECTION</Link>
+          </li>
+          <li className="text-white hover:text-[--mainColor] transition-all duration-150">
+            <Link to="/aboutus">ABOUT US</Link>
+          </li>
+          <li className="text-white hover:text-[--mainColor] transition-all duration-150">
+            <Link to="/contactus">CONTACT US</Link>
+          </li>
+          <li className="text-white hover:text-[--mainColor] transition-all duration-150">
+            <Link to="/register">REGISTER</Link>
+          </li>
         </ul>
+        <div>
+          <ul className="flex gap-4 lg:gap-5">
+            <FontAwesomeIcon
+              className="text-xl lg:text-2xl text-white hover:text-[--mainColor] transition-all duration-150"
+              icon={faMagnifyingGlass}
+            />
+            <FontAwesomeIcon
+              className="text-xl lg:text-2xl text-white hover:text-[--mainColor] transition-all duration-150"
+              icon={faCartPlus}
+            />
+            <FontAwesomeIcon
+              className="text-xl lg:text-2xl text-white hover:text-[--mainColor] transition-all duration-150"
+              icon={faCaretDown}
+            />
+          </ul>
+        </div>
       </div>
     </div>
   );
